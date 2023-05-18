85458593996e01d453899b41e0f95aed0d6536f8
marcoiglesiaslp <entrincov_88@hotmail.com>
2023-05-17 22:01:34 -0300

    add files

--- forms/contact.php
+++ forms/contact.php
@@ -1,37 +1,41 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $email_to = 'leandroeguia@gmail.com'; // Replace with your email address
-  $email_subject = $_POST['subject'];
-  $name = $_POST['name'];
-  $email_from = $_POST['email'];
-  $message = $_POST['message'];
+  /**
+  * Requires the "PHP Email Form" library
+  * The "PHP Email Form" library is available only in the pro version of the template
+  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
+  * For more info and help: https://bootstrapmade.com/php-email-form/
+  */
 
-  // Prepare email headers
-  $headers = "From: $name <$email_from>" . "\r\n";
-  $headers .= "Reply-To: $email_from" . "\r\n";
-  $headers .= "Content-type: text/html" . "\r\n";
+  // Replace contact@example.com with your real receiving email address
+  $receiving_email_address = 'contact@example.com';
 
-  // Send email using Formspree endpoint
-  $fields = array(
-    'name' => $name,
-    '_replyto' => $email_from,
-    'message' => $message
-  );
-  $fields_string = http_build_query($fields);
-  $ch = curl_init('https://formspree.io/f/mwkjzbya'); // Replace with your Formspree endpoint
-  curl_setopt($ch, CURLOPT_POST, 1);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
-  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
-  $result = curl_exec($ch);
-  curl_close($ch);
-
-  // Check if email was sent successfully
-  if ($result == "OK") {
-    echo "Thanks for your message! We'll be in touch soon.";
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
   } else {
-    echo "Sorry, there was an error sending your message. Please try again later.";
+    die( 'Unable to load the "PHP Email Form" Library!');
   }
-}
+
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
+
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  /*
+  $contact->smtp = array(
+    'host' => 'example.com',
+    'username' => 'example',
+    'password' => 'pass',
+    'port' => '587'
+  );
+  */
+
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
+
+  echo $contact->send();
 ?>
